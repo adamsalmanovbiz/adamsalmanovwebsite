3c0d6e19a3fb21dc468df8871628744193d76ada
Adam Salmanov <adam@salmanovmedia.com>
2025-08-17 20:28:14 -0400

    Fix router base for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
 import AdamSalmanov from './AdamSalmanov.jsx';
 import Articles from './Articles.jsx';
 import ArticlePage from './ArticlePage.jsx';
@@ -9,12 +8,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/adamsalmanovwebsite">
       <Routes>
-  <Route path="/" element={<AdamSalmanov />} />
-  <Route path="/articles" element={<Articles />} />
-  <Route path="/articles/:slug" element={<ArticlePage />} />
+        <Route path="/" element={<AdamSalmanov />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/articles/:slug" element={<ArticlePage />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
