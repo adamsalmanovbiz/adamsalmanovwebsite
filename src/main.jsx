1eb49ab9105b9fbdd48146567b3ad42fcb0b6832
Adam Salmanov <adam@salmanovmedia.com>
2025-08-23 17:40:26 -0400

    domain stuff

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import AdamSalmanov from './AdamSalmanov.jsx';
 import Articles from './Articles.jsx';
 import ArticlePage from './ArticlePage.jsx';
+
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
