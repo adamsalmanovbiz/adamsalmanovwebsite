b2238c499ab34dccb6504c4e66f872204a604bbd
Adam Salmanov <adam@salmanovmedia.com>
2025-08-22 22:14:16 -0400

    added /

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import './index.css';
 import AdamSalmanov from './AdamSalmanov.jsx';
 import Articles from './Articles.jsx';
 import ArticlePage from './ArticlePage.jsx';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/adamsalmanovwebsite">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<AdamSalmanov />} />
         <Route path="/articles" element={<Articles />} />
@@ -16,6 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         {/* catch-all so weird paths don’t black-screen */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
